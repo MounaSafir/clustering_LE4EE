b854f676e7c61021e779b53b09d152f62795d7b9
Augustin Laouar <augustinlaouar@hotmail.com>
2026-03-06 11:55:38 +0100

    choose which simulation start

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import simpy
 import random
 import matplotlib.pyplot as plt
+import argparse
 
 from simu.environment import Environment
 from simu.config import Config
@@ -53,133 +54,106 @@ def compute_lifetime(metrics, nb_nodes):
 
     return fnd, hnd, lnd
 
+def run_and_analyze(SensorClass, generator, name, seed=42):
+    env = run_simulation(SensorClass, generator, seed)
+    metrics = env.metric.data
+    nb_nodes = len(env.sensors)
+    fnd, hnd, lnd = compute_lifetime(metrics, nb_nodes)
+    delivery_ratio = final_delivery_ratio(metrics)
+    return {
+        'name': name,
+        'metrics': metrics,
+        'nb_nodes': nb_nodes,
+        'fnd': fnd,
+        'hnd': hnd,
+        'lnd': lnd,
+        'delivery_ratio': delivery_ratio
+    }
 
-env_leach = run_simulation(LeachSensors, generate_random, seed=42)
-env_heed  = run_simulation(HeedSensors, generate_random, seed=42)
-env_deecrp = run_simulation(DeecrpSensors, generate_grid, seed=42)
+def display_results(results):
+    for res in results:
+        print(f"\n--- {res['name']} ---")
+        print("Events generated:", res['metrics']["generated_events"])
+        print("Events received by CH:", res['metrics']["received_by_ch"])
+        print("Events received by BS:", res['metrics']["received_by_bs"])
+        print(f"Delivery ratio: {res['delivery_ratio']:.4f}")
+        print("FND:", res['fnd'])
+        print("HND:", res['hnd'] if res['hnd'] is not None else "not reached")
+        print("LND:", res['lnd'] if res['lnd'] is not None else "not reached")
 
-metrics_leach = env_leach.metric.data
-metrics_heed  = env_heed.metric.data
-metrics_deecrp = env_deecrp.metric.data
+    # Plots
+    if len(results) > 0:
+        plt.figure()
+        for res in results:
+            plt.plot(res['metrics']["time"], res['metrics']["delivery_ratio"], label=res['name'])
+        plt.xlabel("Time")
+        plt.ylabel("Delivery Ratio")
+        plt.title("Delivery Ratio Comparison")
+        plt.ylim(0, 1)
+        plt.grid(True)
+        plt.legend()
+        plt.show()
 
-nb_nodes = len(env_leach.sensors)
+        plt.figure()
+        for res in results:
+            plt.plot(res['metrics']["time"], res['metrics']["alive_nodes"], label=res['name'])
+        plt.xlabel("Time")
+        plt.ylabel("Number of Alive Nodes")
+        plt.title("Alive Nodes Over Time")
+        plt.grid(True)
+        plt.legend()
+        plt.show()
 
-fnd_l, hnd_l, lnd_l = compute_lifetime(metrics_leach, nb_nodes)
-fnd_h, hnd_h, lnd_h = compute_lifetime(metrics_heed,  nb_nodes)
-fnd_d, hnd_d, lnd_d = compute_lifetime(metrics_deecrp,  nb_nodes)
+        total_nodes = results[0]['nb_nodes']
+        plt.figure()
+        for res in results:
+            dead = [total_nodes - a for a in res['metrics']["alive_nodes"]]
+            plt.plot(res['metrics']["time"], dead, label=res['name'])
+        plt.xlabel("Time")
+        plt.ylabel("Number of Dead Nodes")
+        plt.title("Dead Nodes Over Time")
+        plt.grid(True)
+        plt.legend()
+        plt.show()
 
+        plt.figure()
+        for res in results:
+            plt.plot(res['metrics']["time"], res['metrics']["received_by_bs_ts"], label=res['name'])
+        plt.xlabel("Time")
+        plt.ylabel("Cumulative Data Received at BS")
+        plt.title("Cumulative Data Delivered to BS")
+        plt.grid(True)
+        plt.legend()
+        plt.show()
 
+        plt.figure()
+        for res in results:
+            plt.plot(res['metrics']["time"], res['metrics']["energy_total"], label=res['name'])
+        plt.xlabel("Time")
+        plt.ylabel("Total Energy (J)")
+        plt.title("Total Network Energy vs Time")
+        plt.grid(True)
+        plt.legend()
+        plt.show()
 
-print("\n--- LEACH ---")
-print("Events generated:", metrics_leach["generated_events"])
-print("Events received by CH:", metrics_leach["received_by_ch"])
-print("Events received by BS:", metrics_leach["received_by_bs"])
-print(f"Delivery ratio: {final_delivery_ratio(metrics_leach):.4f}")
-print("FND:", fnd_l)
-print("HND:", hnd_l if hnd_l is not None else "not reached")
-print("LND:", lnd_l if lnd_l is not None else "not reached")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run WSN simulations")
+    parser.add_argument('-s', '--simulations', nargs='+', choices=['L', 'H', 'D'], required=True,
+                        help="Simulations to run: L for LEACH, H for HEED, D for DEECRP")
+    args = parser.parse_args()
 
+    sensor_classes = {
+        'L': (LeachSensors, generate_random, 'LEACH'),
+        'H': (HeedSensors, generate_random, 'HEED'),
+        'D': (DeecrpSensors, generate_grid, 'DEECRP')
+    }
 
-print("\n--- HEED ---")
-print("Events generated:", metrics_heed["generated_events"])
-print("Events received by CH:", metrics_heed["received_by_ch"])
-print("Events receivedived by BS:", metrics_heed["received_by_bs"])
-print(f"Delivery ratio: {final_delivery_ratio(metrics_heed):.4f}")
-print("FND:", fnd_h)
-print("HND:", hnd_h if hnd_h is not None else "not reached")
-print("LND:", lnd_h if lnd_h is not None else "not reached")
+    results = []
+    for sim in args.simulations:
+        SensorClass, generator, name = sensor_classes[sim]
+        result = run_and_analyze(SensorClass, generator, name)
+        results.append(result)
 
-
-print("\n--- DEECRP ---")
-print("Events generated:", metrics_deecrp["generated_events"])
-print("Events received by CH:", metrics_deecrp["received_by_ch"])
-print("Events receivedived by BS:", metrics_deecrp["received_by_bs"])
-print(f"Delivery ratio: {final_delivery_ratio(metrics_deecrp):.4f}")
-print("FND:", fnd_d)
-print("HND:", hnd_d if hnd_d is not None else "not reached")
-print("LND:", lnd_d if lnd_d is not None else "not reached")
-
-
-# plt.figure()
-# plt.plot(metrics_leach["time"], metrics_leach["energy_avg"], label="LEACH")
-# plt.plot(metrics_heed["time"],  metrics_heed["energy_avg"],  label="HEED")
-# plt.plot(metrics_deecrp["time"],  metrics_deecrp["energy_avg"],  label="DEECRP")
-# plt.xlabel("Time")
-# plt.ylabel("Average Energy")
-# plt.title("Average Energy Comparison")
-# plt.grid(True)
-# plt.legend()
-# plt.show()
-
-
-plt.figure()
-plt.plot(metrics_leach["time"], metrics_leach["delivery_ratio"], label="LEACH")
-plt.plot(metrics_heed["time"],  metrics_heed["delivery_ratio"],  label="HEED")
-plt.plot(metrics_deecrp["time"],  metrics_deecrp["delivery_ratio"],  label="DEECRP")
-plt.xlabel("Time")
-plt.ylabel("Delivery Ratio")
-plt.title("Delivery Ratio Comparison")
-plt.ylim(0, 1)
-plt.grid(True)
-plt.legend()
-plt.show()
-
-
-plt.figure()
-plt.plot(metrics_leach["time"], metrics_leach["alive_nodes"], label="LEACH")
-plt.plot(metrics_heed["time"],  metrics_heed["alive_nodes"],  label="HEED")
-plt.plot(metrics_deecrp["time"], metrics_deecrp["alive_nodes"], label="DEECRP")
-
-plt.xlabel("Time")
-plt.ylabel("Number of Alive Nodes")
-plt.title("Alive Nodes Over Time")
-plt.grid(True)
-plt.legend()
-plt.show()
-
-
-total_nodes = nb_nodes
-
-dead_leach = [total_nodes - a for a in metrics_leach["alive_nodes"]]
-dead_heed  = [total_nodes - a for a in metrics_heed["alive_nodes"]]
-dead_deecrp = [total_nodes - a for a in metrics_deecrp["alive_nodes"]]
-
-plt.figure()
-plt.plot(metrics_leach["time"], dead_leach, label="LEACH")
-plt.plot(metrics_heed["time"],  dead_heed,  label="HEED")
-plt.plot(metrics_deecrp["time"], dead_deecrp, label="DEECRP")
-
-plt.xlabel("Time")
-plt.ylabel("Number of Dead Nodes")
-plt.title("Dead Nodes Over Time")
-plt.grid(True)
-plt.legend()
-plt.show()
-
-
-plt.figure()
-plt.plot(metrics_leach["time"],metrics_leach["received_by_bs_ts"],label="LEACH")
-plt.plot(metrics_heed["time"],metrics_heed["received_by_bs_ts"],label="HEED")
-plt.plot(metrics_deecrp["time"],metrics_deecrp["received_by_bs_ts"],label="DEECRP")
-
-plt.xlabel("Time")
-plt.ylabel("Cumulative Data Received at BS")
-plt.title("Cumulative Data Delivered to BS")
-plt.grid(True)
-plt.legend()
-plt.show()
-
-
-plt.figure()
-plt.plot(metrics_leach["time"], metrics_leach["energy_total"], label="LEACH")
-plt.plot(metrics_heed["time"],  metrics_heed["energy_total"],  label="HEED")
-plt.plot(metrics_deecrp["time"], metrics_deecrp["energy_total"], label="DEECRP")
-
-plt.xlabel("Time")
-plt.ylabel("Total Energy (J)")
-plt.title("Total Network Energy vs Time")
-plt.grid(True)
-plt.legend()
-plt.show()
+    display_results(results)
 
 
